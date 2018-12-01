209b164bbf5d4ba14bb6676849012a0bf21a9464
Nick Pfundstein <nickpfundstein@gmail.com>
2018-12-01 10:18:48 -0800

    Day 1 Part 2 complete

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,8 +1,14 @@
 use std::io::BufReader;
 use std::fs::File;
 use std::io::BufRead;
+use std::collections::HashSet;
 
 fn main() {
+    part1();
+    part2();
+}
+
+fn part1() {
     let input = BufReader::new(File::open("input.txt").expect("File not found!"));
 
     let result: i32 = input.lines()
@@ -11,4 +17,23 @@ fn main() {
         .sum();
 
     println!("{}", result);
+}
+
+fn part2() {
+    let input = BufReader::new(File::open("input.txt").expect("File not found!"));
+
+    let mut current = 0;
+    let mut found: HashSet<i32> = HashSet::new();
+    found.insert(0);
+
+    for x in input.lines().map(|x| x.unwrap()).collect::<Vec<String>>().iter().cycle() {
+        current += x.parse::<i32>().unwrap();
+        if found.contains(&current) {
+            break;
+        } else {
+            found.insert(current);
+        }
+    }
+
+    println!("{}", current);
 }
