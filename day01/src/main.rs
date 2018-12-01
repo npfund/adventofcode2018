c14cb875d58dbbd07e63e08506f5b56058efb812
Nick Pfundstein <nickpfundstein@gmail.com>
2018-12-01 10:08:26 -0800

    Day 1 Part 1 complete

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,3 +1,14 @@
+use std::io::BufReader;
+use std::fs::File;
+use std::io::BufRead;
+
 fn main() {
-    println!("Hello, world!");
+    let input = BufReader::new(File::open("input.txt").expect("File not found!"));
+
+    let result: i32 = input.lines()
+        .map(|x| x.unwrap())
+        .map(|x| x.parse::<i32>().unwrap())
+        .sum();
+
+    println!("{}", result);
 }
