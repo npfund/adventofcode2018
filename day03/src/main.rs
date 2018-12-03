9ac97f2664faa55cc7bdc7c58d0db8dad4512717
Nick Pfundstein <nickpfundstein@gmail.com>
2018-12-03 10:18:55 -0500

    Day 3 Part 1 complete

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,3 +1,34 @@
+extern crate regex;
+
+use std::io::BufReader;
+use std::fs::File;
+use std::io::BufRead;
+use regex::Regex;
+use std::collections::HashMap;
+
 fn main() {
-    println!("Hello, world!");
+    part1();
+}
+
+fn part1() {
+    let input = BufReader::new(File::open("input.txt").expect("File not found!"));
+    let regex = Regex::new(r"#\d+ @ (\d+),(\d+): (\d+)x(\d+)").unwrap();
+    let mut fabric: HashMap<(i32, i32), i32> = HashMap::new();
+
+    for line in input.lines().map(|x| x.unwrap()) {
+        let captures = regex.captures(&line).unwrap();
+        let start = (captures[1].parse::<i32>().unwrap(), captures[2].parse::<i32>().unwrap());
+        let width = captures[3].parse::<i32>().unwrap();
+        let height = captures[4].parse::<i32>().unwrap();
+
+        for h in 0..height {
+            for w in 0..width {
+                let coord = (start.0 + w, start.1 + h);
+                let entry = fabric.entry(coord).or_insert(0);
+                *entry += 1;
+            }
+        }
+    }
+
+    println!("{}", fabric.values().filter(|&&x| x > 1).count());
 }
