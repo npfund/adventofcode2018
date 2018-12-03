863303005d18159757548720acc0a4a3025f3518
Nick Pfundstein <nickpfundstein@gmail.com>
2018-12-03 10:36:18 -0500

    Day 3 Part 2 complete

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -8,6 +8,7 @@ use std::collections::HashMap;
 
 fn main() {
     part1();
+    part2();
 }
 
 fn part1() {
@@ -31,4 +32,48 @@ fn part1() {
     }
 
     println!("{}", fabric.values().filter(|&&x| x > 1).count());
+}
+
+fn part2() {
+    let input = BufReader::new(File::open("input.txt").expect("File not found!"))
+        .lines()
+        .map(|x| x.unwrap())
+        .collect::<Vec<String>>();
+    let regex = Regex::new(r"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)").unwrap();
+    let mut fabric: HashMap<(i32, i32), i32> = HashMap::new();
+
+    for line in input.clone() {
+        let captures = regex.captures(&line).unwrap();
+        let start = (captures[2].parse::<i32>().unwrap(), captures[3].parse::<i32>().unwrap());
+        let width = captures[4].parse::<i32>().unwrap();
+        let height = captures[5].parse::<i32>().unwrap();
+
+        for h in 0..height {
+            for w in 0..width {
+                let coord = (start.0 + w, start.1 + h);
+                let entry = fabric.entry(coord).or_insert(0);
+                *entry += 1;
+            }
+        }
+    }
+
+    'outer: for line in input {
+        let captures = regex.captures(&line).unwrap();
+        let start = (captures[2].parse::<i32>().unwrap(), captures[3].parse::<i32>().unwrap());
+        let width = captures[4].parse::<i32>().unwrap();
+        let height = captures[5].parse::<i32>().unwrap();
+
+        for h in 0..height {
+            for w in 0..width {
+                let coord = (start.0 + w, start.1 + h);
+                let value = fabric.get(&coord).unwrap();
+                if *value > 1 {
+                    continue 'outer;
+                }
+            }
+        }
+
+        println!("{}", captures[1].parse::<i32>().unwrap());
+        return;
+    }
 }
